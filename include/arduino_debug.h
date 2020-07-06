3295107aca09464cc048e407e269e2d6867a86a3
Zachary Rubin <46578381+zjrubin@users.noreply.github.com>
2020-07-05 22:45:06 -0400

    Add rotary encoder switch interrupt handler

---
+++ include/arduino_debug.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <stdio.h>
+
+#ifdef ARDUINO_DEBUG
+#define DEBUG_TEST 1
+#else
+#define DEBUG_TEST 0
+#endif
+
+#define DEBUG_OUTPUT_MAX_SIZE 100
+
+#define debug_serial_printfln(message, ...)                   \
+    do                                                        \
+    {                                                         \
+        if (DEBUG_TEST)                                       \
+        {                                                     \
+            char debug_output_buf[DEBUG_OUTPUT_MAX_SIZE];     \
+            sprintf(debug_output_buf, "%s line:%d: " message, \
+                    __func__, __LINE__, __VA_ARGS__);         \
+            Serial.println(debug_output_buf);                 \
+        }                                                     \
+    } while (0)
+
+#define debug_serial_println(message) \
+    do                                \
+    {                                 \
+        if (DEBUG_TEST)               \
+        {                             \
+            Serial.println(message);  \
+        }                             \
+    } while (0)
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <Arduino.h>
 
+#include "arduino_debug.h"
+
 #define NUM_ELEMENTS(x) \
-    ((sizeof(x) / sizeof(0 [x])) / ((size_t)(!(sizeof(x) % sizeof(0 [x])))))
+  ((sizeof(x) / sizeof(0 [x])) / ((size_t)(!(sizeof(x) % sizeof(0 [x])))))
 
 #define NIXIE_ZERO 0b0000
 #define NIXIE_ONE 0b1000
@@ -18,12 +20,15 @@
 #define LEFT_DISPLAY(nixie_digit) (nixie_digit << 4)
 #define RIGHT_DISPLAY(nixie_digit) (nixie_digit)
 #define DUAL_DISPLAY(left_nixie_digit, right_nixie_digit) \
-    (LEFT_DISPLAY(left_nixie_digit) | RIGHT_DISPLAY(right_nixie_digit))
+  (LEFT_DISPLAY(left_nixie_digit) | RIGHT_DISPLAY(right_nixie_digit))
 
-const uint8_t nixie_digits[] = {NIXIE_ZERO, NIXIE_ONE, NIXIE_TWO,
-                                NIXIE_THREE,
-                                NIXIE_FOUR, NIXIE_FIVE, NIXIE_SIX,
-                                NIXIE_SEVEN, NIXIE_EIGHT, NIXIE_NINE};
+const uint8_t nixie_digits[] = {NIXIE_ZERO,  NIXIE_ONE,  NIXIE_TWO, NIXIE_THREE,
+                                NIXIE_FOUR,  NIXIE_FIVE, NIXIE_SIX, NIXIE_SEVEN,
+                                NIXIE_EIGHT, NIXIE_NINE};
+
+#define DEBOUNCE_TIME_MS 150
+#define DEBOUNCE_TIME_TICKS (DEBOUNCE_TIME_MS / portTICK_PERIOD_MS)
+volatile TickType_t previous_tick_count = 0;
 
 constexpr int c_clock_pin = 12;
 constexpr int c_latch_pin = 14;
@@ -32,36 +37,73 @@ constexpr int c_data_pin = 26;
 
 constexpr int c_upper_left_dot = 25;
 
+const int c_rotary_encoder_switch_pin = 2;
+
+TaskHandle_t g_task_configure_handle = NULL;
+
+SemaphoreHandle_t g_semaphore_configure = xSemaphoreCreateBinary();
+
+void task_configure(void* pvParameters);
 void shift_out_nixie_digit(uint8_t digit);
+void rotary_encoder_switch_isr();
 
-void setup()
-{
-    pinMode(c_clock_pin, OUTPUT);
-    pinMode(c_latch_pin, OUTPUT);
-    pinMode(c_output_enable_pin, OUTPUT);
-    pinMode(c_data_pin, OUTPUT);
-    pinMode(c_upper_left_dot, OUTPUT);
+void setup() {
+  // Nixie setup
+  pinMode(c_clock_pin, OUTPUT);
+  pinMode(c_latch_pin, OUTPUT);
+  pinMode(c_output_enable_pin, OUTPUT);
+  pinMode(c_data_pin, OUTPUT);
+  pinMode(c_upper_left_dot, OUTPUT);
 
-    digitalWrite(c_output_enable_pin, LOW);
+  digitalWrite(c_output_enable_pin, LOW);
+
+  // Rotary encode setup
+  pinMode(c_rotary_encoder_switch_pin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(c_rotary_encoder_switch_pin),
+                  rotary_encoder_switch_isr, FALLING);
+
+  Serial.begin(BAUD_RATE);
+
+  xTaskCreate(task_configure, "configure", configMINIMAL_STACK_SIZE, NULL, 1,
+              &g_task_configure_handle);
 }
 
-void loop()
-{
-    for (int i = 0; i < NUM_ELEMENTS(nixie_digits); ++i)
-    {
-        shift_out_nixie_digit(i);
-        delay(500);
-        digitalWrite(c_upper_left_dot, !digitalRead(c_upper_left_dot));
+// Idle task
+void loop() {
+  for (int i = 0; i < NUM_ELEMENTS(nixie_digits); ++i) {
+    shift_out_nixie_digit(i);
+    delay(500);
+    digitalWrite(c_upper_left_dot, !digitalRead(c_upper_left_dot));
+  }
+}
+
+void task_configure(void* pvParameters) {
+  for (;;) {
+    if (xSemaphoreTake(g_semaphore_configure, portMAX_DELAY) == pdTRUE) {
+      debug_serial_println("Configuration Menu:");
+      //   while (true) {
+      //     Serial.println("HERE");
+      //   }
     }
+  }
 }
 
-void shift_out_nixie_digit(uint8_t digit)
-{
-    digitalWrite(c_latch_pin, LOW);
+void shift_out_nixie_digit(uint8_t digit) {
+  digitalWrite(c_latch_pin, LOW);
 
-    uint8_t display_digit = LEFT_DISPLAY(nixie_digits[digit]);
+  uint8_t display_digit = LEFT_DISPLAY(nixie_digits[digit]);
 
-    shiftOut(c_data_pin, c_clock_pin, LSBFIRST, display_digit);
+  shiftOut(c_data_pin, c_clock_pin, LSBFIRST, display_digit);
 
-    digitalWrite(c_latch_pin, HIGH);
+  digitalWrite(c_latch_pin, HIGH);
+}
+
+void rotary_encoder_switch_isr() {
+  TickType_t current_tick_count = xTaskGetTickCountFromISR();
+  if (current_tick_count - previous_tick_count > DEBOUNCE_TIME_TICKS) {
+    previous_tick_count = current_tick_count;
+    debug_serial_println("rotary_encoder_switch_isr");
+    xSemaphoreGiveFromISR(g_semaphore_configure, NULL);
+    portYIELD_FROM_ISR();
+  }
 }
