6d503d01203788032c6e03c230165dbc8e641cdb
Zachary Rubin <46578381+zjrubin@users.noreply.github.com>
2020-07-11 21:05:14 -0400

    Update smooth transition to turn off old digit first

--- src/main.cpp
+++ src/main.cpp
@@ -20,16 +20,23 @@
 #define NIXIE_SEVEN 0b1110
 #define NIXIE_EIGHT 0b0001
 #define NIXIE_NINE 0b1001
-#define NIXIE_BLANK 0b1111
+#define NIXIE_BLANK_CODE 0b1111
+
+#define NUM_NIXIE_DIGITS 10
 
 #define LEFT_DISPLAY(nixie_digit) (nixie_digit << 4)
 #define RIGHT_DISPLAY(nixie_digit) (nixie_digit)
 #define DUAL_DISPLAY(left_nixie_digit, right_nixie_digit) \
   (LEFT_DISPLAY(left_nixie_digit) | RIGHT_DISPLAY(right_nixie_digit))
 
-const uint8_t nixie_digits[] = {NIXIE_ZERO,  NIXIE_ONE,  NIXIE_TWO, NIXIE_THREE,
-                                NIXIE_FOUR,  NIXIE_FIVE, NIXIE_SIX, NIXIE_SEVEN,
-                                NIXIE_EIGHT, NIXIE_NINE};
+const uint8_t nixie_digits[] = {NIXIE_ZERO,  NIXIE_ONE,       NIXIE_TWO,
+                                NIXIE_THREE, NIXIE_FOUR,      NIXIE_FIVE,
+                                NIXIE_SIX,   NIXIE_SEVEN,     NIXIE_EIGHT,
+                                NIXIE_NINE,  NIXIE_BLANK_CODE};
+
+#define NIXIE_BLANK NUM_ELEMENTS(nixie_digits)
+
+#define MILLISECOND_TO_MICROSECONDS 1000
 
 #define DEBOUNCE_TIME_MS 200
 #define DEBOUNCE_TIME_TICKS (DEBOUNCE_TIME_MS / portTICK_PERIOD_MS)
@@ -57,6 +64,8 @@ void task_cycle_digit(void* pvParameters);
 void task_configure(void* pvParameters);
 void smooth_transition_nixie_digit(uint8_t next_digit, uint8_t current_digit,
                                    size_t transition_time_ms);
+void _smooth_transition_helper(uint8_t next_digit, uint8_t current_digit,
+                               size_t transition_time_ms);
 void shift_out_nixie_digit(uint8_t digit);
 void rotary_encoder_switch_isr();
 uint8_t get_config_value(uint8_t initial_value);
@@ -100,9 +109,8 @@ void loop() {}
 
 void task_cycle_digit(void* pvParameters) {
   for (;;) {
-    for (size_t i = 0; i < NUM_ELEMENTS(nixie_digits); ++i) {
-      smooth_transition_nixie_digit((i + 1) % NUM_ELEMENTS(nixie_digits), i,
-                                    1000);
+    for (size_t i = 0; i < NUM_NIXIE_DIGITS; ++i) {
+      smooth_transition_nixie_digit((i + 1) % NUM_NIXIE_DIGITS, i, 1000);
       // shift_out_nixie_digit(i);
       // delay(500);
       digitalWrite(c_upper_left_dot, !digitalRead(c_upper_left_dot));
@@ -134,23 +142,31 @@ void task_configure(void* pvParameters) {
 
 void smooth_transition_nixie_digit(uint8_t next_digit, uint8_t current_digit,
                                    size_t transition_time_ms) {
+  _smooth_transition_helper(NIXIE_BLANK, current_digit,
+                            transition_time_ms / 2.0);
+  _smooth_transition_helper(next_digit, NIXIE_BLANK, transition_time_ms / 2.0);
+}
+
+void _smooth_transition_helper(uint8_t next_digit, uint8_t current_digit,
+                               size_t transition_time_ms) {
   size_t multiplex_count = 100;
   double single_digit_transition_time =
       transition_time_ms / (double)multiplex_count;
 
   for (size_t i = 0; i < multiplex_count; ++i) {
     double current_digit_display_proportion =
-        (multiplex_count - i) / (double)multiplex_count;
-    double next_digit_display_proportion = i / (double)multiplex_count;
+        0.5 * (cos(PI * (i / (double)multiplex_count)) + 1);
+    double next_digit_display_proportion = 1 - current_digit_display_proportion;
 
     shift_out_nixie_digit(current_digit);
     delayMicroseconds(
         (current_digit_display_proportion * single_digit_transition_time) *
-        1000);
+        MILLISECOND_TO_MICROSECONDS);
 
     shift_out_nixie_digit(next_digit);
     delayMicroseconds(
-        (next_digit_display_proportion * single_digit_transition_time) * 1000);
+        (next_digit_display_proportion * single_digit_transition_time) *
+        MILLISECOND_TO_MICROSECONDS);
   }
 }
 
