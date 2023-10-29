cfee6587bdd860082db49c067e9167732fbd1e38
Zachary Rubin <46578381+zjrubin@users.noreply.github.com>
2023-10-28 23:40:46 -0400

    Move wifi credentials to credentials.cpp

--- include/ntp.h
+++ include/ntp.h
@@ -3,8 +3,6 @@
 #define NTP_OK 0
 #define NTP_ERROR -1
 
-extern const char* const c_wifi_ssid;
-extern const char* const c_wifi_password;
 extern const char* const c_ntp_server;
 extern const long int c_gmt_offset_sec;
 extern const int c_daylight_offset_sec;
---
+++ src/credentials.cpp
@@ -0,0 +1,4 @@
+#include "credentials.h"
+
+const char* const c_wifi_ssid = "YOUR_WIFI_SSID";
+const char* const c_wifi_password = "YOUR_WIFI_PASSWORD";
