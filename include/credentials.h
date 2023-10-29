a5e3f83824ed37e05c69912999ebaa094f72d2a1
Zachary Rubin <46578381+zjrubin@users.noreply.github.com>
2023-10-29 00:17:59 -0400

    Add constant for OpenWeather API key

--- include/credentials.h
+++ include/credentials.h
@@ -1,2 +1,9 @@
+#ifndef __CREDENTIALS_H__
+#define __CREDENTIALS_H__
+
 extern const char* const c_wifi_ssid;
 extern const char* const c_wifi_password;
+
+extern const char* const c_open_weather_api_key;
+
+#endif
