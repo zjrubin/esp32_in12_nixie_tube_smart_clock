6f76fc44ebeda7530963d247bdf74f5db57e4301
Zachary Rubin <46578381+zjrubin@users.noreply.github.com>
2023-10-29 21:15:51 -0400

    Add Nixie_Display::smooth_display_value
     - Add smooth transition for nixie dots
     - Allow setting blank digits from array

--- src/Nixie_Display.cpp
+++ src/Nixie_Display.cpp
@@ -4,22 +4,17 @@
 
 #include "util.h"
 
-const uint8_t Nixie_Display::nixie_digits[] = {
+const uint8_t Nixie_Display::nixie_digits[NUM_NIXIE_DIGITS] = {
     NIXIE_ZERO,  NIXIE_ONE,  NIXIE_TWO,       NIXIE_THREE,
     NIXIE_FOUR,  NIXIE_FIVE, NIXIE_SIX,       NIXIE_SEVEN,
     NIXIE_EIGHT, NIXIE_NINE, NIXIE_BLANK_CODE};
-
-const uint8_t Nixie_Display::nixie_dots[] = {
-    NIXIE_DOTS_NONE,  NIXIE_DOTS_ALL, NIXIE_DOTS_LEFT,
-    NIXIE_DOTS_RIGHT, NIXIE_DOTS_TOP, NIXIE_DOTS_BOTTOM};
 
 const int Nixie_Display::clock_pin = 12;
 const int Nixie_Display::latch_pin = 14;
 const int Nixie_Display::output_enable_pin = 27;
 const int Nixie_Display::data_pin = 26;
 
-uint8_t Nixie_Display::m_digits[num_display_digits] = {
-    NIXIE_ZERO, NIXIE_ZERO, NIXIE_ZERO, NIXIE_ZERO, NIXIE_ZERO, NIXIE_ZERO};
+uint8_t Nixie_Display::m_digits[num_display_digits] = {NIXIE_ZERO};
 uint8_t Nixie_Display::m_dots = NIXIE_DOTS_ALL;
 
 SemaphoreHandle_t Nixie_Display::display_mutex = NULL;
@@ -42,40 +37,67 @@ void Nixie_Display::smooth_display_time(const struct tm& current_time,
   struct tm next_time;
   get_offset_time(&next_time, current_time, 1);
 
-  uint8_t current_time_arr[num_display_digits];
+  uint8_t hours = next_time.tm_hour;
+  if (twelve_hour_format) {
+    hours = convert_24_hour_to_12_hour(hours);
+  }
+
+  // The transition happens imperceptibly faster than 1 second.
+  // this ensures that the display time task can call vTaskDelayUntil
+  // and wake up exactly 1 second after it was initially called.
+  // This prevents time drift appearing on the display.
+  smooth_display_value(NIXIE_SMOOTH_TRANSITION_TIME_MS, hours, next_time.tm_min,
+                       next_time.tm_sec, nixie_dots, false);
+}
+
+void Nixie_Display::smooth_display_value(size_t transition_time_ms,
+                                         int8_t hours, int8_t minutes,
+                                         int8_t seconds, uint8_t nixie_dots,
+                                         bool blank_all) {
+  uint8_t current_value_arr[num_display_digits];
   // We need to create an intermediate time struct. If any digit changed
   // from the current second to the next, that digit needs to fade to blank
   // first before the new digit appears
   uint8_t intermediate_blanked_arr[num_display_digits];
-  uint8_t next_time_arr[num_display_digits];
+  uint8_t next_value_arr[num_display_digits];
 
-  set_time_in_array(current_time_arr, current_time, twelve_hour_format);
-  set_time_in_array(intermediate_blanked_arr, next_time, twelve_hour_format);
-  set_time_in_array(next_time_arr, next_time, twelve_hour_format);
+  memcpy(current_value_arr, m_digits, sizeof(m_digits));
+  set_digit_array_from_value(next_value_arr, hours, minutes, seconds);
 
-  // Determine which digits changed from the current time to the next time,
-  // and thus need to be blanked
-  for (size_t i = 0; i < num_display_digits; ++i) {
-    if (current_time_arr[i] != next_time_arr[i]) {
-      intermediate_blanked_arr[i] = NIXIE_BLANK_POS;
+  uint8_t current_dots = m_dots;
+  uint8_t intermediate_dots = nixie_dots;
+
+  if (blank_all) {
+    memset(intermediate_blanked_arr, NIXIE_BLANK_POS,
+           sizeof(intermediate_blanked_arr));
+    intermediate_dots = NIXIE_DOTS_NONE;
+  } else {
+    // Determine which digits changed from the current time to the next time,
+    // and thus need to be blanked
+    set_digit_array_from_value(intermediate_blanked_arr, hours, minutes,
+                               seconds);
+    for (size_t i = 0; i < num_display_digits; ++i) {
+      if (current_value_arr[i] != next_value_arr[i]) {
+        intermediate_blanked_arr[i] = NIXIE_BLANK_POS;
+      }
+    }
+    if (current_dots != nixie_dots) {
+      intermediate_dots = NIXIE_DOTS_NONE;
     }
   }
 
-  m_dots = nixie_dots;
-
-  // The transition happens imperceptibly faster than 1 second.
-  // this ensures that the display time task can call vTaskDelayUntil
-  // and wake up exactly 1 second after it was initally called.
-  // This prevents time drift appearing on the display.
-  smooth_display_transition(current_time_arr, intermediate_blanked_arr,
-                            NIXIE_SMOOTH_TRANSITION_TIME_MS / 2);
-  smooth_display_transition(intermediate_blanked_arr, next_time_arr,
-                            NIXIE_SMOOTH_TRANSITION_TIME_MS / 2);
+  smooth_display_transition(current_value_arr, intermediate_blanked_arr,
+                            current_dots, intermediate_dots,
+                            transition_time_ms / 2);
+  smooth_display_transition(intermediate_blanked_arr, next_value_arr,
+                            intermediate_dots, nixie_dots,
+                            transition_time_ms / 2);
 }
 
 void Nixie_Display::smooth_display_transition(
     const uint8_t current_digits[num_display_digits],
-    const uint8_t next_digits[num_display_digits], size_t transition_time_ms) {
+    const uint8_t next_digits[num_display_digits], uint8_t current_nixie_dots,
+    uint8_t next_nixie_dots, size_t transition_time_ms) {
   size_t multiplex_count = 100;
   double single_digit_transition_time =
       transition_time_ms / (double)multiplex_count;
@@ -86,12 +108,14 @@ void Nixie_Display::smooth_display_transition(
     double next_digit_display_proportion = 1 - current_digit_display_proportion;
 
     memcpy(m_digits, current_digits, sizeof(m_digits));
+    m_dots = current_nixie_dots;
     show();
     delayMicroseconds(
         (current_digit_display_proportion * single_digit_transition_time) *
         MILLISECOND_TO_MICROSECONDS);
 
     memcpy(m_digits, next_digits, sizeof(m_digits));
+    m_dots = next_nixie_dots;
     show();
     delayMicroseconds(
         (next_digit_display_proportion * single_digit_transition_time) *
@@ -110,15 +134,10 @@ void Nixie_Display::display_time(const struct tm& time_info,
 
 void Nixie_Display::display_date(const struct tm& time_info,
                                  uint8_t nixie_dots) {
-  int month = time_info.tm_mon + 1;  // tm_month starts at 0
-  m_digits[0] = TENS(month);
-  m_digits[1] = ONES(month);
+  uint8_t month = time_info.tm_mon + 1;  // tm_month starts at 0
 
-  m_digits[2] = TENS(time_info.tm_mday);
-  m_digits[3] = ONES(time_info.tm_mday);
-
-  m_digits[4] = TENS(time_info.tm_year);
-  m_digits[5] = ONES(time_info.tm_year);
+  set_digit_array_from_value(m_digits, month, time_info.tm_mday,
+                             time_info.tm_year);
 
   m_dots = nixie_dots;
 
@@ -196,14 +215,7 @@ void Nixie_Display::display_slot_machine_cycle(const struct tm& current_time,
 
 void Nixie_Display::display_value(uint8_t hours, uint8_t minutes,
                                   uint8_t seconds, uint8_t nixie_dots) {
-  m_digits[0] = TENS(hours);
-  m_digits[1] = ONES(hours);
-
-  m_digits[2] = TENS(minutes);
-  m_digits[3] = ONES(minutes);
-
-  m_digits[4] = TENS(seconds);
-  m_digits[5] = ONES(seconds);
+  set_digit_array_from_value(m_digits, hours, minutes, seconds);
 
   m_dots = nixie_dots;
 
@@ -272,14 +284,15 @@ void Nixie_Display::set_time_in_array(uint8_t array[num_display_digits],
     hours = convert_24_hour_to_12_hour(hours);
   }
 
-  array[0] = TENS(hours);
-  array[1] = ONES(hours);
+  set_digit_array_from_value(array, hours, time_info.tm_min, time_info.tm_sec);
+}
 
-  array[2] = TENS(time_info.tm_min);
-  array[3] = ONES(time_info.tm_min);
-
-  array[4] = TENS(time_info.tm_sec);
-  array[5] = ONES(time_info.tm_sec);
+void Nixie_Display::get_current_display(uint8_t* hours, uint8_t* minutes,
+                                        uint8_t* seconds, uint8_t* dots) const {
+  *hours = (10 * m_digits[0]) + m_digits[1];
+  *minutes = (10 * m_digits[2]) + m_digits[3];
+  *seconds = (10 * m_digits[4]) + m_digits[5];
+  *dots = m_dots;
 }
 
 void Nixie_Display::get_offset_time(struct tm* offset_time,
@@ -293,4 +306,32 @@ void Nixie_Display::get_offset_time(struct tm* offset_time,
   time_t next_time_epoch = mktime(offset_time);
 
   localtime_r(&next_time_epoch, offset_time);
+}
+
+void Nixie_Display::set_digit_array_from_value(
+    uint8_t digit_array[Nixie_Display::num_display_digits], int8_t hours,
+    int8_t minutes, int8_t seconds) {
+  if (hours == NIXIE_BLANK_DIGIT) {
+    digit_array[0] = NIXIE_BLANK_POS;
+    digit_array[1] = NIXIE_BLANK_POS;
+  } else {
+    digit_array[0] = TENS(hours);
+    digit_array[1] = ONES(hours);
+  }
+
+  if (minutes == NIXIE_BLANK_DIGIT) {
+    digit_array[2] = NIXIE_BLANK_POS;
+    digit_array[3] = NIXIE_BLANK_POS;
+  } else {
+    digit_array[2] = TENS(minutes);
+    digit_array[3] = ONES(minutes);
+  }
+
+  if (seconds == NIXIE_BLANK_DIGIT) {
+    digit_array[4] = NIXIE_BLANK_POS;
+    digit_array[5] = NIXIE_BLANK_POS;
+  } else {
+    digit_array[4] = TENS(seconds);
+    digit_array[5] = ONES(seconds);
+  }
 }
